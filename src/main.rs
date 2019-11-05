8349bab81747a8baf97a0aa182a09fe113446043
andrew dunnells <andrew@polychora.net>
2019-11-04 22:34:29 -0600

    Added enum and matching function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
     println!("Hello, world!");
+    let mut state = State::On;
+    state = flip(state);
+    match state{
+        State::On => (println!("Something bad happened")),
+        State::Off => (println!("All is well"))
+    }
+}
+
+fn flip(state: State) -> State
+{
+    match state{
+        State::On => (return State::Off),
+        State::Off => (return State::On)
+    };
+}
+
+enum State
+{
+    On,
+    Off 
 }
