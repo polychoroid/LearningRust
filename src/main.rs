34861c18f19af942851e419b366ee177cc95a2ec
andrew dunnells <andrew@polychora.net>
2019-11-23 16:51:17 -0600

    Added some game of life features

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,44 @@ fn main() {
 fn flip(state: State) -> State
 {
     match state{
-        State::On => (return State::Off),
-        State::Off => (return State::On)
-    };
+        State::On => State::Off,
+        State::Off => State::On
+    }
 }
 
 enum State
 {
-    On,
-    Off 
+    On = 1,
+    Off = 0 
+}
+
+pub struct Universe
+{
+    width: u32,
+    height: u32,
+    cells: Vec<State>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let index = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[index] as u8;
+            }
+        }
+    count
+    }
 }
